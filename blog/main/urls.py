069bb8483fb7c460c4cd5245242168f184e315cd
Object626 <Shtokov6161@gmail.com>
2023-08-03 17:28:06 +0400

    настроил цвета на главной странице и фоновое изображение

--- blog/main/urls.py
+++ blog/main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-
+    path('Articles/', views.index, name='Articles'),
+    path('author/', views.index, name='author')
 ]
